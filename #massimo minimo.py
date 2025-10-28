7622a7153b8ea0836273d06565678055c7d6d4cd
Aleksander.Manca <Aleksander@gmail.com>
2025-10-28 21:14:07 +0100

    massimo e minimo

---
+++ #massimo minimo.py
@@ -0,0 +1,11 @@
+#massimo minimo.py
+n1=float(input("Inserisci un numero: "))
+n2=float(input("Inserisci un secondo numero: "))
+n3=float(input("Inserisci un terzo numero: "))
+massimo=n1
+if (massimo>n2) and (massimo>n3):
+    print("Il massimo numero è", massimo)
+elif (massimo<n2) and (massimo>n3):
+    print("Il massimo numero è", n2)
+if (massimo>n2) and (massimo<n3):
+    print("Il massimo numero è", n3) 
